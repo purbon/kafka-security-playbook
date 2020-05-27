48ecc38e059b2d912eb154e5b0db78ccaeff41f6
Pere Urbon <pere@confluent.io>
2020-05-27 16:51:21 +0200

    add client examples with using kafka producer and consumer with schema registry using mtls as authentication method in the rest api

---
+++ schema-registry/clients-examples/src/main/java/com/purbon/kafka/tls/KafkaConsumerExample.java
@@ -0,0 +1,61 @@
+package com.purbon.kafka.tls;
+
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import java.util.Arrays;
+import java.util.Properties;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.Serdes;
+
+public class KafkaConsumerExample {
+
+  public static Properties config() {
+    Properties props = new Properties();
+
+    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    props.put(ConsumerConfig.GROUP_ID_CONFIG, "group"+System.currentTimeMillis());
+    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, Serdes.String().deserializer().getClass());
+    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
+    props.put("schema.registry.url", "https://localhost:8099");
+
+    props.put("schema.registry.ssl.truststore.location", "secrets/client.truststore");
+    props.put("schema.registry.ssl.truststore.password", "confluent");
+    props.put("schema.registry.ssl.keystore.location", "secrets/client.keystore");
+    props.put("schema.registry.ssl.keystore.password", "confluent");
+    props.put("schema.registry.ssl.key.password", "confluent");
+
+    props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+    return props;
+  }
+
+  private static String TOPIC = "topic.avro";
+
+  public static void main(String[] args) {
+
+    final Consumer<String, GenericRecord> consumer = new KafkaConsumer<String, GenericRecord>(config());
+    consumer.subscribe(Arrays.asList(TOPIC));
+
+    try {
+      while (true) {
+        ConsumerRecords<String, GenericRecord> records = consumer.poll(100);
+        for (ConsumerRecord<String, GenericRecord> record : records) {
+          System.out.printf("offset = %d, key = %s, value = %s \n", record.offset(), record.key(), record.value());
+        }
+      }
+    } finally {
+      consumer.close();
+    }
+
+  }
+}
---
+++ schema-registry/clients-examples/src/main/java/com/purbon/kafka/tls/KafkaProducerExample.java
@@ -0,0 +1,56 @@
+package com.purbon.kafka.tls;
+
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import java.util.Properties;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.Serdes;
+
+public class KafkaProducerExample {
+
+  public static Properties config() {
+    Properties props = new Properties();
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, Serdes.String().serializer().getClass());
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+    props.put("schema.registry.url", "https://localhost:8099");
+
+    props.put("schema.registry.ssl.truststore.location", "secrets/client.truststore");
+    props.put("schema.registry.ssl.truststore.password", "confluent");
+    props.put("schema.registry.ssl.keystore.location", "secrets/client.keystore");
+    props.put("schema.registry.ssl.keystore.password", "confluent");
+    props.put("schema.registry.ssl.key.password", "confluent");
+    return props;
+  }
+
+  private static String OUTPUT_TOPIC = "topic.avro";
+
+  public static void main(String[] args) {
+
+    KafkaProducer producer = new KafkaProducer(config());
+
+    String key = "key1";
+    String userSchema = "{\"type\":\"record\"," +
+        "\"name\":\"myrecord\"," +
+        "\"fields\":[{\"name\":\"f1\",\"type\":\"string\"}]}";
+
+    Schema.Parser parser = new Schema.Parser();
+    Schema schema = parser.parse(userSchema);
+
+    String[] values = new String[]{ "value1", "value2", "value3"};
+
+    for(String value : values) {
+      GenericRecord avroRecord = new GenericData.Record(schema);
+      avroRecord.put("f1", value);
+      ProducerRecord<Object, Object> record = new ProducerRecord<Object, Object>(OUTPUT_TOPIC, key, avroRecord);
+      producer.send(record);
+    }
+    producer.flush();
+    producer.close();
+
+  }
+}
